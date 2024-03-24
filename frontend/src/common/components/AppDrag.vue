7a6b3e722f5aac1025c5d5f53de02ebfe7acdab6
nBogdanov <bogdanow.ns2016@yandex.ru>
2024-03-24 16:17:38 +0500

    Добавил компоненты-обёртки для dnd

---
+++ frontend/src/common/components/AppDrag.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    :draggable="true"
+    @dragstart.self="onDrag"
+    @dragover.prevent
+    @dragenter.prevent
+  ></div>
+  <slot />
+</template>
+
+<script setup>
+import { DATA_TRANSFER_PAYLOAD, MOVE } from "../constants";
+const props = defineProps({
+  transferData: {
+    type: Object,
+    required: true,
+  },
+});
+
+function onDrag({ dataTransfer }) {
+  dataTransfer.effectAllowerd = MOVE;
+  dataTransfer.dropEffect = MOVE;
+  dataTransfer.setData(
+    DATA_TRANSFER_PAYLOAD,
+    JSON.stringify(props.transferData)
+  );
+}
+</script>
+
+<style lang="" scoped></style>
